aa0dd98a0bfd99c9647410d2cd77e12a8d858cdd
Anjan R Athreya <anjan.r.athreya@gmail.com>
2025-04-03 16:34:06 -0400

    Initial Commit

---
+++ CircleRun/CircleRunApp.swift
@@ -0,0 +1,17 @@
+//
+//  CircleRunApp.swift
+//  CircleRun
+//
+//  Created by Anjan Athreya on 4/3/25.
+//
+
+import SwiftUI
+
+@main
+struct CircleRunApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CircleRun/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CircleRun
+//
+//  Created by Anjan Athreya on 4/3/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
